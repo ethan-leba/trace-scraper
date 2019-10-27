3fcd9735490adefde9c44145ee40e666eb8ef775
ethan-leba <ethanleba5@gmail.com>
2019-10-27 09:45:18 -0400

    collects instructor names

--- index.js
+++ index.js
@@ -1,62 +1,100 @@
 const puppeteer = require('puppeteer');
+const { Cluster } = require('puppeteer-cluster');
+
 const CREDS = require('./creds');
 
 async function run() {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
+    console.log("Launching chromium...");
+	const browser = await puppeteer.launch({
+		headless: false
+	});
+	const page = await browser.newPage();
 
-    await page.goto('http://my.northeastern.edu/c/portal/login');
-    await page.waitFor(1000);
+	await page.goto('http://my.northeastern.edu/c/portal/login');
+	await page.waitFor(1000);
 
-    // logins
-    // login selectors
-    const USERNAME_SELECTOR = '#username';
-    const PASSWORD_SELECTOR = '#password';
-    const BUTTON_SELECTOR = 'body > section > div > div:nth-child(1) > div > form > div:nth-child(3) > button';
-    // login logic
-    await page.click(USERNAME_SELECTOR);
-    await page.keyboard.type(CREDS.username);
+	// logins
+	// login selectors
+	const USERNAME_SELECTOR = '#username';
+	const PASSWORD_SELECTOR = '#password';
+	const BUTTON_SELECTOR = 'body > section > div > div:nth-child(1) > div > form > div:nth-child(3) > button';
+    console.log("Logging into myNEU...");
+	// login logic
+	await page.click(USERNAME_SELECTOR);
+	await page.keyboard.type(CREDS.username);
 
-    await page.click(PASSWORD_SELECTOR);
-    await page.keyboard.type(CREDS.password);
+	await page.click(PASSWORD_SELECTOR);
+	await page.keyboard.type(CREDS.password);
 
-    await page.click(BUTTON_SELECTOR);
+	await page.click(BUTTON_SELECTOR);
 
-    await page.waitForNavigation();
+	await page.waitForNavigation();
 
-    // get on to the trace website
-    await page.waitForSelector('#portlet_com_neu_events_display_portlet_EventsDisplayPortlet_INSTANCE_BL8mcIrWtJXq > div > div > div > div > div.container-header.mobile-collapse > a');
+	// get on to the trace website
+	await page.waitForSelector('#portlet_com_neu_events_display_portlet_EventsDisplayPortlet_INSTANCE_BL8mcIrWtJXq > div > div > div > div > div.container-header.mobile-collapse > a');
 
-    await page.goto('https://www.applyweb.com/eval/shibboleth/neu/36892');
-    await page.waitForSelector('#navbar > ul > li:nth-child(3) > a');
-    await page.goto('https://www.applyweb.com/eval/new/reportbrowser');
+    console.log("Launching TRACE website...");
+	await page.goto('https://www.applyweb.com/eval/shibboleth/neu/36892');
+	await page.waitForSelector('#navbar > ul > li:nth-child(3) > a');
+	await page.goto('https://www.applyweb.com/eval/new/reportbrowser');
 
-    // we can now scrape
-    
-    // pull out the table from the page
-    await page.waitForSelector('iframe');
- //   await page.waitFor(10 * 1000);
-    const iframe = await page.mainFrame().childFrames()[0];
-    // waits for content inside of the row to appear
-    await iframe.waitForSelector('td.ng-binding');
-    await page.waitFor(1000);
-    const table = await iframe.$$('tr');
+	// we can now scrape
 
-    let links = []
-    for(i = 0; i < table.length; i++) {
-        const link = await table[i].$('a');
-        if(link) {
-            links.push(link);
-        }
-        //console.log(await table[i].$eval('a', link => link.innerText));
+    console.log("Collecting links on page 1");
+	// pull out the table from the page
+	await page.waitForSelector('iframe');
+	const iframe = await page.mainFrame().childFrames()[0];
+	// waits for content inside of the row to appear
+	await iframe.waitForSelector('td.ng-binding');
+	await page.waitFor(1000);
+	const table = await iframe.$$('tbody tr');
+
+	let urls = []
+	for(i = 0; i < table.length; i++) {
+		const link = await table[i].$('a');
+		if(link) {
+			urls.push(await iframe.evaluate(a => a.href, link));
+		}
+	}
+	console.log(urls);
+    for(i = 0; i < urls.length; i++) {
+		await page.goto(urls[i]);
+        await page.waitForSelector('iframe');
+        const iframe = await page.mainFrame().childFrames()[0];
+        // waits for content inside of the row to appear
+        const sel = '#tapestryContainer > div.container-fluid > div.row > div > div > div.col-xs-6.text-left > ul > li:nth-child(1)';
+        await iframe.waitForSelector(sel);
+//        await page.waitFor(500);
+        const element = await iframe.$(sel);
+        const text = await iframe.evaluate(element => element.textContent, element);
+        console.log(text);
     }
-    console.log(urls);
-    //await page.waitForSelector('#sort-pages', { visible: true, timeout: 0 });   
-    await page.screenshot({ path: 'screenshots/github.png' });
+    /*
+	const cluster = await Cluster.launch({
+		concurrency: Cluster.CONCURRENCY_CONTEXT,
+		maxConcurrency: 5,
+	});
 
-    browser.close();
+	await cluster.task(async ({ page, data: url }) => {
+		await page.goto(url);
+        await page.waitForSelector('iframe');
+        const iframe = await page.mainFrame().childFrames()[0];
+        // waits for content inside of the row to appear
+        await iframe.waitForSelector('#tapestryContainer > div.container-fluid > div.row > div > div > div.col-xs-6.text-left > ul');
+		await page.screenshot({'path': url});
+		// Store screenshot, do something else
+	});
+
+	for(i = 0; i < urls.length; i++) {
+	    cluster.queue(urls[i]);
+	}*/
+	// many more pages
+
+	await cluster.idle();
+	await cluster.close();
+	//await page.screenshot({ path: 'screenshots/github.png' });
+
+	browser.close();
 }
 
 run();
